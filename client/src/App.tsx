f6697c16c84250230f83e6b7a5394746740baac1
Ignacio Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2024-10-17 19:18:39 -0300

    Conexión frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Container, Stack } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+
+export const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "/api";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Stack h="100vh">
+      <Navbar />
+      <Container>
+        <TodoForm />
+        <TodoList />
+      </Container>
+    </Stack>
+  );
 }
 
-export default App
+export default App;
--- main.go
+++ main.go
@@ -7,30 +7,29 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
-type Todo struct{
-	// Define el campo ID como entero y el campo Title como una cadena, json:"id" significa q al representarse en json, sea como "id"
-	// bson es para mongoDB como el campo completo se va a llamar id
-	// primitive.ObjectID es un tipo de dato de mongoDB, cambié int por eso.
-	ID primitive.ObjectID `json:"id,omitempty" bson:"id,omitempty"`
-	Title string `json:"title"`
-	Completed bool `json:"completed"`
-	Body string `json:"body"`
+
+type Todo struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Completed bool               `json:"completed"`
+	Body      string             `json:"body"`
 }
 
 var collection *mongo.Collection
 
-func main(){
-	fmt.Println("Hello FRESH")
-    err := godotenv.Load(".env")
-    if err!= nil {
-        log.Fatal("Error loading.env file")
-    }
+func main() {
+	if os.Getenv("ENV") != "production" {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env file:", err)
+		}
+	}
 
 	MONGODB_URI := os.Getenv("MONGODB_URI")
 	clientOptions := options.Client().ApplyURI(MONGODB_URI)
@@ -39,16 +38,24 @@ func main(){
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer client.Disconnect(context.Background())
 
-	if err = client.Ping(context.Background(), nil); err != nil {
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Connected to MongoDB!")
+
+	fmt.Println("Connected to MONGODB ATLAS")
 
 	collection = client.Database("golang_db").Collection("todos")
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowHeaders: "Origin,Content-Type,Accept",
+	}))
 
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodo)
@@ -57,45 +64,51 @@ func main(){
 
 	port := os.Getenv("PORT")
 	if port == "" {
-        port = "4000"
-    }
+		port = "5000"
+	}
 
-	log.Fatal(app.Listen("0.0.0.0:"+port))
+	if os.Getenv("ENV") == "production" {
+		app.Static("/", "./client/dist")
+	}
+
+	log.Fatal(app.Listen("0.0.0.0:" + port))
+
 }
 
 func getTodos(c *fiber.Ctx) error {
-	//bson.M{} es un filtro vacio, por lo que se traen todos los documentos
-	cursor, err := collection.Find(context.Background(), bson.M{})
-	if err != nil {
-		log.Fatal(err)
-	}
 	var todos []Todo
+
+	cursor, err := collection.Find(context.Background(), bson.M{})
+
+	if err != nil {
+		return err
+	}
 
 	defer cursor.Close(context.Background())
 
 	for cursor.Next(context.Background()) {
 		var todo Todo
-        err := cursor.Decode(&todo)
-        if err!= nil {
-            log.Fatal(err)
-        }
-        todos = append(todos, todo)
-    }
-	return c.JSON(todos)		
+		if err := cursor.Decode(&todo); err != nil {
+			return err
+		}
+		todos = append(todos, todo)
+	}
+
+	return c.JSON(todos)
 }
 
 func createTodo(c *fiber.Ctx) error {
 	todo := new(Todo)
+	// {id:0,completed:false,body:""}
+
 	if err := c.BodyParser(todo); err != nil {
 		return err
 	}
-	//Esto es igual a if todo.ID != 0
-	if todo.ID != primitive.NilObjectID {
-		return c.Status(400).JSON(fiber.Map{"error": "ID must not be set"})
+
+	if todo.Body == "" {
+		return c.Status(400).JSON(fiber.Map{"error": "Todo body cannot be empty"})
 	}
-	if todo.Title == ""{
-		return c.Status(400).JSON(fiber.Map{"error": "Title is required"})
-	}
+
 	insertResult, err := collection.InsertOne(context.Background(), todo)
 	if err != nil {
 		return err
@@ -103,30 +116,43 @@ func createTodo(c *fiber.Ctx) error {
 
 	todo.ID = insertResult.InsertedID.(primitive.ObjectID)
 
-	return c.JSON(todo)
+	return c.Status(201).JSON(todo)
 }
 
 func updateTodo(c *fiber.Ctx) error {
-	id, err := primitive.ObjectIDFromHex(c.Params("id"))
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+
 	if err != nil {
-		return c.Status(400).JSON(fiber.Map{"error": "Invalid ID"})
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid todo ID"})
 	}
-	//colección actualizar uno, argumentos -> contexto, que voy a actualizar, que voy a cambiar en el documento
-	_ , err = collection.UpdateOne(context.Background(), bson.M{"_id": id}, bson.M{"$set": bson.M{"completed": true}})
+
+	filter := bson.M{"_id": objectID}
+	update := bson.M{"$set": bson.M{"completed": true}}
+
+	_, err = collection.UpdateOne(context.Background(), filter, update)
 	if err != nil {
 		return err
 	}
-	return c.JSON(fiber.Map{"message": "Todo updated successfully"})
+
+	return c.Status(200).JSON(fiber.Map{"success": true})
+
 }
 
 func deleteTodo(c *fiber.Ctx) error {
-	id, err := primitive.ObjectIDFromHex(c.Params("id"))
-    if err != nil {
-        return c.Status(400).JSON(fiber.Map{"error": "Invalid ID"})
-    }
-    _, err = collection.DeleteOne(context.Background(), bson.M{"_id": id})
-    if err != nil {
-        return err
-    }
-    return c.JSON(fiber.Map{"message": "Todo deleted successfully"})
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid todo ID"})
+	}
+
+	filter := bson.M{"_id": objectID}
+	_, err = collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{"success": true})
 }
